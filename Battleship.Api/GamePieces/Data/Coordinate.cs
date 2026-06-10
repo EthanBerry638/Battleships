a88bc4355ab6e383513489bd00c9a955d26ce50d
Ethan Berry <ethanberry670@gmail.com>
2026-06-10 23:15:22 +0100

    Fix: Coordinate now immutable

--- Battleship.Api/GamePieces/Data/Coordinate.cs
+++ Battleship.Api/GamePieces/Data/Coordinate.cs
@@ -2,8 +2,8 @@
 {
     public class Coordinate
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        public int X { get; }
+        public int Y { get; }
 
         public Coordinate(int x, int y)
         {
