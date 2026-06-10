5384f4065364efd9fa286564b42a1768276bc114
Ethan Berry <ethanberry670@gmail.com>
2026-06-10 23:14:55 +0100

    Feat: Add board drawing logic in constructor for game board

--- Battleship.Api/GamePieces/Board/GameBoard.cs
+++ Battleship.Api/GamePieces/Board/GameBoard.cs
@@ -3,5 +3,16 @@
     public class GameBoard
     {
         private readonly Tile[,] _board = new Tile[10, 10];
+
+        public GameBoard()
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    _board[x, y] = new Tile();
+                }
+            }
+        }  
     }
 }
