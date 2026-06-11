c9220f629af6466926f2b8bd008348d1243dea48
Ethan Berry <ethanberry670@gmail.com>
2026-06-11 12:00:12 +0100

    Fix: Now passing all tests

--- Battleship.Api/Parsers/CoordinateParser.cs
+++ Battleship.Api/Parsers/CoordinateParser.cs
@@ -1,4 +1,5 @@
 ﻿using Battleship.Api.GamePieces.Data;
+using Battleship.Api.Exceptions;    
 
 namespace Battleship.Api.Parsers
 {
@@ -7,6 +8,11 @@ namespace Battleship.Api.Parsers
         public Coordinate StringToCoord(string input)
         {
             input = input.ToUpper();
+
+            if (input[0] > 'J' || int.Parse(input[1..]) > 10)
+            {
+                throw new InvalidCoordinateException($"Invalid coordinate: {input}");
+            }
 
             int x = (int)input[0] - (int)'A';
             int y = int.Parse(input[1..]) - 1;
