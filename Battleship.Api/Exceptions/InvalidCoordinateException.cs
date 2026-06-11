e925e9249b8c2835f6b7a7d408be03b943560534
Ethan Berry <ethanberry670@gmail.com>
2026-06-11 11:43:20 +0100

    Feat: Add custom exception

---
+++ Battleship.Api/Exceptions/InvalidCoordinateException.cs
@@ -0,0 +1,8 @@
+﻿namespace Battleship.Api.Exceptions
+{
+    public class InvalidCoordinateException : Exception
+    {
+        public InvalidCoordinateException(){}
+        public InvalidCoordinateException(string message):base(message){}
+    }
+}
