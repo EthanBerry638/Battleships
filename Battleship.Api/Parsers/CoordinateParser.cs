6c63d8704c7dfb30729382efda380ab515047e61
Ethan Berry <ethanberry670@gmail.com>
2026-06-11 10:30:28 +0100

    Feat: Add method template and first failing test

--- Battleship.Api/Parsers/CoordinateParser.cs
+++ Battleship.Api/Parsers/CoordinateParser.cs
@@ -1,7 +1,12 @@
-﻿namespace Battleship.Api.Parsers
+﻿using Battleship.Api.GamePieces.Data;
+
+namespace Battleship.Api.Parsers
 {
     public class CoordinateParser
     {
-
+        public Coordinate StringToCoord(string input)
+        {
+            return null;
+        }
     }
 }
---
+++ Battleship.Tests/Parser Tests/CoordinateParserTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Battleship.Api.Parsers;
+using Battleship.Api.GamePieces.Data;
+using FluentAssertions;
+
+namespace Battleship.Tests.Parser_Tests
+{
+    public class CoordinateParserTests
+    {
+        [Fact]
+        public void StringToCoord_ReturnsCorrectCoordinates_WhenGivenValidInput()
+        {
+            var parser = new CoordinateParser();
+            var input = "A1";
+            var expectedCoordinates = new Coordinate(0, 0);
+
+            var result = parser.StringToCoord(input);
+
+            result.Should().BeEquivalentTo(expectedCoordinates);
+        }
+    }
+}
