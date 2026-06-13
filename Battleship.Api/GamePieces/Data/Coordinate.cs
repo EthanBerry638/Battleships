ceb2d7d6dc0c3adea00e4f009613867805d7bf2c
Ethan Berry <ethanberry670@gmail.com>
2026-06-13 13:44:53 +0100

    Update: Coordinate is now a record

--- Battleship.Api/GamePieces/Data/Coordinate.cs
+++ Battleship.Api/GamePieces/Data/Coordinate.cs
@@ -1,13 +1,4 @@
 ﻿namespace Battleship.Api.GamePieces.Data
 {
-    public class Coordinate (int x, int y)
-    {
-        public int X { get; } = x;
-        public int Y { get; } = y;
-
-        public override string ToString()
-        {
-            return $"({X}, {Y})";
-        }
-    }
+    public record Coordinate (int X, int Y) {}
 }
