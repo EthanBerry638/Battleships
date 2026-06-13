986ab6d87d6d9f75dcb40d424273d83b17178fa8
Ethan Berry <ethanberry670@gmail.com>
2026-06-13 13:28:35 +0100

    Feat: Add new test case for when only some cooridnates are hit

--- Battleship.Tests/Ship Tests/ShipTests.cs
+++ Battleship.Tests/Ship Tests/ShipTests.cs
@@ -7,7 +7,7 @@ namespace Battleship.Tests.Ship_Tests;
 public class ShipTests
 {
     [Fact]
-    public void IsSunk_ReturnsTrue_AfterRegisteringHitOnOnlyCoordinate()
+    public void IsSunk_ReturnsTrue_AfterRegisteringHitOnOnlyOneCoordinate()
     {
         var coord = new Coordinate(0, 0);
         var ship = new Ship([coord]);
@@ -15,5 +15,17 @@ public class ShipTests
         ship.RegisterHit(coord);
         
         ship.IsSunk().Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsSunk_ReturnsFalse_WhenOnlySomeCoordinatesAreHit()
+    {
+        var coord1 = new Coordinate(0, 0);
+        var coord2 = new Coordinate(0, 1);
+        var ship = new Ship([coord1, coord2]);
+        
+        ship.RegisterHit(coord1);
+        
+        ship.IsSunk().Should().BeFalse();
     }
 }
