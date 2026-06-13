2f49cf6132d2bcc1ca36a46b1945343572a6a259
Ethan Berry <ethanberry670@gmail.com>
2026-06-13 12:42:15 +0100

    Feat: Add first test case for shoot

--- Battleship.Api/Engine/BattleshipEngine.cs
+++ Battleship.Api/Engine/BattleshipEngine.cs
@@ -1,9 +1,15 @@
 ﻿using Battleship.Api.GamePieces.Board;
+using Battleship.Api.GamePieces.Data;
 
 namespace Battleship.Api.Engine
 {
     public class BattleshipEngine (IGameBoard gameBoard)
     {
         private readonly IGameBoard _gameBoard = gameBoard;
+        
+        public ShotResult Shoot(Coordinate coordinate)
+        {
+            return ShotResult.Hit;
+        }
     }
 }
--- Battleship.Tests/Engine Tests/BattleshipEngineTests.cs
+++ Battleship.Tests/Engine Tests/BattleshipEngineTests.cs
@@ -1,6 +1,9 @@
 using Battleship.Api.GamePieces.Board;
 using Moq;
 using Battleship.Api.Engine;
+using Battleship.Api.GamePieces.Data;
+using Battleship.Api.GamePieces.Entities;
+using FluentAssertions;
 
 namespace Battleship.Tests.Engine_Tests
 {
@@ -14,5 +17,17 @@ namespace Battleship.Tests.Engine_Tests
             _mockGameBoard = new Mock<IGameBoard>();
             _battleshipEngine = new BattleshipEngine(_mockGameBoard.Object);
         }
+
+        [Fact]
+        public void Shoot_ReturnsHit_WhenTileHasShip()
+        {
+            var coordinate = new Coordinate(0, 0);
+            var tile = new Tile { OccupyingShip = new Ship() };
+            _mockGameBoard.Setup(x => x.GetTile(coordinate)).Returns(tile);
+            
+            var result = _battleshipEngine.Shoot(coordinate);
+            
+            result.Should().Be(ShotResult.Hit);
+        }   
     }
 }
