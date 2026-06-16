c6f5c9dce9dd5d848b047fb9263a56c1ce23822a
Ethan Berry <ethanberry670@gmail.com>
2026-06-16 16:04:24 +0100

    Feat: Now passing new test. However, I am moving the null check to the ship constructor

--- Battleship.Api/GamePieces/Board/GameBoard.cs
+++ Battleship.Api/GamePieces/Board/GameBoard.cs
@@ -31,6 +31,11 @@ namespace Battleship.Api.GamePieces.Board
 
         public PlacementResult PlaceShip(Ship ship)
         {
+            if (ship?.Coordinates == null || ship.Coordinates.Count == 0)
+            {
+                return new PlacementResult(false);
+            }
+            
             foreach (var coordinate in ship.Coordinates)
             {
                 if (GetTile(coordinate).OccupyingShip != null)
