22b12642e268f410c7e7fd61b1e2504f70aa1dda
Ethan Berry <ethanberry670@gmail.com>
2026-06-19 15:01:45 +0100

    Fix: Now passing new test for unordered coordinates

--- Battleship.Api/GamePieces/Entities/Ship.cs
+++ Battleship.Api/GamePieces/Entities/Ship.cs
@@ -14,8 +14,8 @@ namespace Battleship.Api.GamePieces.Entities
         {
             ArgumentNullException.ThrowIfNull(coordinates);
             ValidateCoordinates(coordinates);
-            
-            Coordinates = coordinates;
+
+            Coordinates = coordinates.OrderBy(c => c.X).ThenBy(c => c.Y).ToList();
         }
 
         public void RegisterHit(Coordinate coordinate)
@@ -30,33 +30,35 @@ namespace Battleship.Api.GamePieces.Entities
         {
             return _hits.Count == Coordinates.Count;
         }
-        
+
         //TODO: Add validation for ship size against the ship type
-        
+
         private void ValidateCoordinates(List<Coordinate> coordinates)
         {
             if (coordinates.Count <= 1) throw new InvalidShipException("A ship must occupy at least 2 coordinates.");
-            
-            var firstCoord = coordinates[0];
-            bool isHorizontal = coordinates.All(c => c.Y == firstCoord.Y);
-            bool isVertical = coordinates.All(c => c.X == firstCoord.X);
-            bool isDuplicated = coordinates.Distinct().Count() != coordinates.Count;
+
+            var sortedCoordinates = coordinates.OrderBy(c => c.X).ThenBy(c => c.Y).ToList();
+
+            var firstCoord = sortedCoordinates[0];
+            bool isHorizontal = sortedCoordinates.All(c => c.Y == firstCoord.Y);
+            bool isVertical = sortedCoordinates.All(c => c.X == firstCoord.X);
+            bool isDuplicated = sortedCoordinates.Distinct().Count() != sortedCoordinates.Count;
 
             if (isDuplicated) throw new InvalidShipException("Coordinates must be unique.");
-            
+
             if (!isHorizontal && !isVertical)
             {
                 throw new InvalidShipException("Coordinates must be in a straight line.");
             }
 
-            for (int i = 0; i < coordinates.Count - 1; i++)
+            for (int i = 0; i < sortedCoordinates.Count - 1; i++)
             {
-                var currentCoordinate = coordinates[i];
-                var nextCoordinate = coordinates[i + 1];
-                
+                var currentCoordinate = sortedCoordinates[i];
+                var nextCoordinate = sortedCoordinates[i + 1];
+
                 var xDifference = Math.Abs(currentCoordinate.X - nextCoordinate.X);
                 var yDifference = Math.Abs((currentCoordinate.Y - nextCoordinate.Y));
-                
+
                 if (xDifference != 1 && yDifference != 1)
                 {
                     throw new InvalidShipException("Coordinates must be adjacent.");
