df2dc1b9e8272229c07219df8c41f75a890dc5b7
EthanBerry638 <ethanberry670@gmail.com>
2026-07-04 18:08:13 +0100

    Update: Ship interface now has it's type

--- Battleship.Api/GamePieces/Entities/IShip.cs
+++ Battleship.Api/GamePieces/Entities/IShip.cs
@@ -4,6 +4,7 @@ namespace Battleship.Api.GamePieces.Entities;
 
 public interface IShip
 {
+    ShipType Type { get; }
     void RegisterHit(Coordinate coordinate);
     bool IsSunk();
     List<Coordinate> Coordinates { get; }
