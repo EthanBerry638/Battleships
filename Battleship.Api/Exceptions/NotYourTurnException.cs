25ccae9b39aebf15fc8036c867a997c2047b1c52
EthanBerry638 <ethanberry670@gmail.com>
2026-07-09 19:25:33 +0100

    Feat: Add not your turn exception

---
+++ Battleship.Api/Exceptions/NotYourTurnException.cs
@@ -0,0 +1,7 @@
+﻿namespace Battleship.Api.Exceptions;
+
+public class NotYourTurnException : Exception
+{
+    public NotYourTurnException(){}
+    public NotYourTurnException(string message):base(message){}   
+}
