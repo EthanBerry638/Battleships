cf548b7622faec6a7f5fcab935d46bc6ba90ed55
EthanBerry638 <ethanberry670@gmail.com>
2026-07-09 13:59:19 +0100

    Feat: Add game start result wrapper

---
+++ Battleship.Api/GamePieces/Data/GameStartResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Battleship.Api.GamePieces.Data;
+
+public record GameStartResult
+{
+    public bool Success { get; }
+    public FleetValidationResult[]? ValidationErrors { get; }
+
+    private GameStartResult(bool success, FleetValidationResult[]? errors)
+    {
+        Success = success;
+        ValidationErrors = errors;
+    }
+
+    public static GameStartResult Ok() => new(true, null);
+    public static GameStartResult Invalid(FleetValidationResult[] errors) => new(false, errors);
+}
