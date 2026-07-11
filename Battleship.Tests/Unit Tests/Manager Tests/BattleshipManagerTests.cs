8e86acb6c63dad3839b4bad4cdafeb4167079588
EthanBerry638 <ethanberry670@gmail.com>
2026-07-11 17:09:04 +0100

    Refactor: Completed switch to factory

--- Battleship.Tests/Unit Tests/Manager Tests/BattleshipManagerTests.cs
+++ Battleship.Tests/Unit Tests/Manager Tests/BattleshipManagerTests.cs
@@ -1,17 +1,36 @@
 ﻿using Battleship.Api.Services;
 using Battleship.Api.Engine;
 using FluentAssertions;
+using Battleship.Api.GamePieces.Board;
+using Battleship.Api.GamePieces.Entities;
+using Moq;
 
 namespace Battleship.Tests.Unit_Tests.Manager_Tests;
 
 public class GameManagerTests
 {
+    private readonly BattleshipManager _manager;
+
+    public GameManagerTests()
+    {
+        var mockFactory = new Mock<IBattleshipEngineFactory>();
+        
+        var mockEngine = new Mock<BattleshipEngine>(
+            new Mock<IGameBoard>().Object, 
+            new Mock<IGameBoard>().Object, 
+            new Mock<Player>("Player 1").Object, 
+            new Mock<Player>("Player 2").Object
+        );
+
+        mockFactory.Setup(f => f.Create()).Returns(mockEngine.Object);
+
+        _manager = new BattleshipManager(mockFactory.Object);
+    }
+
     [Fact]
     public void CreateGame_ShouldReturnSixCharacterCode_WhenCalled()
     {
-        var manager = new BattleshipManager();
-        
-        string result = manager.CreateGame();
+        string result = _manager.CreateGame();
         
         result.Length.Should().Be(6);
         result.Should().NotBeNull();
@@ -20,10 +39,8 @@ public class GameManagerTests
     [Fact]
     public void CreateGame_ShouldReturnUniqueCodes_WhenCalledMultipleTimes()
     {
-        var manager = new BattleshipManager();
-
-        string code1 = manager.CreateGame();
-        string code2 = manager.CreateGame();
+        string code1 = _manager.CreateGame();
+        string code2 = _manager.CreateGame();
 
         code1.Should().NotBe(code2);
     }
@@ -31,9 +48,7 @@ public class GameManagerTests
     [Fact]
     public void CreateGame_ShouldReturnUppercaseCode_WhenCalled()
     {
-        var manager = new BattleshipManager();
-        
-        string result = manager.CreateGame();
+        string result = _manager.CreateGame();
         
         result.Should().BeUpperCased();
     }
@@ -41,10 +56,9 @@ public class GameManagerTests
     [Fact]
     public void GetGame_ShouldReturnEngineInstance_WhenGameExists()
     {
-        var manager = new BattleshipManager();
-        string gameCode = manager.CreateGame();
+        string gameCode = _manager.CreateGame();
         
-        var result = manager.GetGame(gameCode);
+        var result = _manager.GetGame(gameCode);
         
         result.Should().NotBeNull();
     }
