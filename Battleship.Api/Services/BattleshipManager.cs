c8a900280f15b6ccd54ae9abcba55628177b7163
EthanBerry638 <ethanberry670@gmail.com>
2026-07-11 17:02:41 +0100

    WIP: Battleship engine factory is now used in the manager. Note: Tests not compiling right now due to this.

--- Battleship.Api/Services/BattleshipManager.cs
+++ Battleship.Api/Services/BattleshipManager.cs
@@ -1,25 +1,24 @@
 ﻿using System.Collections.Concurrent;
-using Battleship.Api.GamePieces.Entities;
-using Battleship.Api.GamePieces.Board;
 using Battleship.Api.Engine;
 
 namespace Battleship.Api.Services;
 
-public class BattleshipManager
+public class BattleshipManager (IBattleshipEngineFactory engineFactory)
 {
     private readonly ConcurrentDictionary<string, BattleshipEngine> _games = new();
+    private readonly IBattleshipEngineFactory _engineFactory = engineFactory;
 
     public string CreateGame()
     {
         string gameCode = Guid.NewGuid().ToString("N")[..6].ToUpper();
-
-        var engine = new BattleshipEngine(new GameBoard(), new GameBoard(), new Player("placeholder"), new Player("placeholder"));
+        
+        var engine = _engineFactory.Create();
         _games.TryAdd(gameCode, engine);
         
         return gameCode;
     }
     
-    public BattleshipEngine GetGame(string gameCode)
+    public BattleshipEngine? GetGame(string gameCode)
     {
         _games.TryGetValue(gameCode, out var engine);
         return engine;
