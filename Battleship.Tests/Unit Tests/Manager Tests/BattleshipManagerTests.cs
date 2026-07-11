f2b61fac014d2f33861c9f1c9e590845f83b3cf6
EthanBerry638 <ethanberry670@gmail.com>
2026-07-11 14:13:46 +0100

    Feat: Add new failing test

---
+++ Battleship.Tests/Unit Tests/Manager Tests/BattleshipManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using Battleship.Api.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace Battleship.Tests.Unit_Tests.Manager_Tests;
+
+public class GameManagerTests
+{
+    [Fact]
+    public void CreateGame_ShouldReturnSixCharacterCode_WhenCalled()
+    {
+        var manager = new BattleshipManager();
+        
+        string result = manager.CreateGame();
+        
+        result.Length.Should().Be(6);
+        result.Should().NotBeNull();
+    }
+}
