9e9ccb7292ff1a9a2d2b6dccc812d5cd24d3c258
EthanBerry638 <ethanberry670@gmail.com>
2026-07-11 17:00:15 +0100

    WIP: Add concrete factory implementation for BattleshipEngine

---
+++ Battleship.Api/Services/BattleshipEngineFactory.cs
@@ -0,0 +1,31 @@
+﻿using Battleship.Api.Engine;
+using Battleship.Api.GamePieces.Board;
+using Battleship.Api.GamePieces.Entities;
+
+namespace Battleship.Api.Services;
+
+public class BattleshipEngineFactory : IBattleshipEngineFactory
+{
+    private readonly IGameBoard _player1Board;
+    private readonly IGameBoard _player2Board;
+    private readonly Player _player1;
+    private readonly Player _player2;
+
+    public BattleshipEngineFactory(IGameBoard player1Board, IGameBoard player2Board, Player player1, Player player2)
+    {
+        ArgumentNullException.ThrowIfNull(player1Board);
+        ArgumentNullException.ThrowIfNull(player2Board);
+        ArgumentNullException.ThrowIfNull(player1);
+        ArgumentNullException.ThrowIfNull(player2);
+
+        _player1Board = player1Board;
+        _player2Board = player2Board;
+        _player1 = player1;
+        _player2 = player2;
+    }
+
+    public BattleshipEngine Create()
+    {
+        return new BattleshipEngine(_player1Board, _player2Board, _player1, _player2);
+    }
+}
