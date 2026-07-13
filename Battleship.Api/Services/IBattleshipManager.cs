f8de24a089df6e096e851f0e3e03674c567f73f9
EthanBerry638 <ethanberry670@gmail.com>
2026-07-13 15:13:06 +0100

    Update interface to include create game

--- Battleship.Api/Services/IBattleshipManager.cs
+++ Battleship.Api/Services/IBattleshipManager.cs
@@ -1,8 +1,10 @@
 ﻿using Battleship.Api.Engine;
+using Battleship.Api.GamePieces.Entities;
 
 namespace Battleship.Api.Services;
 
 public interface IBattleshipManager
 {
+    string CreateGame (Player player1, Player player2);
     BattleshipEngine? GetGame (string gameCode);
 }
