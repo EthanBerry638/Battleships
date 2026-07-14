b63c38c1f9596f2ef7c941dfe71ee28c1ce4579f
EthanBerry638 <ethanberry670@gmail.com>
2026-07-14 20:17:15 +0100

    Update: Tests that depend on create lobby deleted for now, kept tests updated

--- Battleship.Tests/Unit Tests/Manager Tests/BattleshipManagerTests.cs
+++ Battleship.Tests/Unit Tests/Manager Tests/BattleshipManagerTests.cs
@@ -11,83 +11,51 @@ public class GameManagerTests
     private readonly Player _dummyPlayer2 = new("Player 2");
 
     [Fact]
-    public void CreateGame_ShouldReturnSixCharacterCode_WhenCalled()
+    public void CreateLobby_ShouldReturnSixCharacterCode_WhenCalled()
     {
-        string result = _manager.CreateGame(_dummyPlayer1, _dummyPlayer2);
-        
+        string result = _manager.CreateLobby(_dummyPlayer1);
+
         result.Length.Should().Be(6);
         result.Should().NotBeNull();
     }
-    
+
     [Fact]
-    public void CreateGame_ShouldReturnUniqueCodes_WhenCalledMultipleTimes()
+    public void CreateLobby_ShouldReturnUniqueCodes_WhenCalledMultipleTimes()
     {
-        string code1 = _manager.CreateGame(_dummyPlayer1, _dummyPlayer2);
-        string code2 = _manager.CreateGame(_dummyPlayer1, _dummyPlayer2);
+        string code1 = _manager.CreateLobby(_dummyPlayer1);
+        string code2 = _manager.CreateLobby(_dummyPlayer1);
 
         code1.Should().NotBe(code2);
     }
-    
+
     [Fact]
-    public void CreateGame_ShouldReturnUppercaseCode_WhenCalled()
+    public void CreateLobby_ShouldReturnUppercaseCode_WhenCalled()
     {
-        string result = _manager.CreateGame(_dummyPlayer1, _dummyPlayer2);
-        
+        string result = _manager.CreateLobby(_dummyPlayer1);
+
         result.Should().BeUpperCased();
     }
-    
-    [Theory]
-    [MemberData(nameof(NullPlayerData))]
-    public void CreateGame_ShouldThrowArgumentNullException_WhenEitherPlayerIsNull(Player? player1, Player? player2)
-    {
-        var act = () => _manager.CreateGame(player1!, player2!);
-
-        act.Should()
-            .Throw<ArgumentNullException>();
-    }
-
-    public static IEnumerable<object?[]> NullPlayerData =>
-    [
-        [null, new Player("Player 2")],
-        [new Player("Player 1"), null],
-        [null, null]
-    ];
-    
-    [Fact]
-    public void GetGame_ShouldReturnEngineInstance_WhenGameExists()
-    {
-        string gameCode = _manager.CreateGame(_dummyPlayer1, _dummyPlayer2);
-        
-        var result = _manager.GetGame(gameCode);
-        
-        result.Should().NotBeNull();
-    }
 
     [Fact]
-    public void GetGame_ShouldReturnMultipleEngineInstances_WhenCalledMultipleTimesOnExistingGames()
+    public void CreateLobby_ShouldThrowArgumentNullException_WhenPlayerIsNull()
     {
-        for (int i = 0; i < 10; i++)
-        {
-            string gameCode = _manager.CreateGame(_dummyPlayer1, _dummyPlayer2);
-            
-            var result = _manager.GetGame(gameCode);
-            
-            result.Should().NotBeNull();
-        }
+        var act = () => _manager.CreateLobby(null!);
+
+        act.Should().Throw<ArgumentNullException>();
     }
-    
+
     [Theory]
-    [InlineData("123456")]   
-    [InlineData("ABCDEF")]  
-    [InlineData("123-ABC")]  
+    [InlineData("123456")]
+    [InlineData("ABCDEF")]
+    [InlineData("123-ABC")]
     [InlineData("abcdef")]
-    [InlineData("")]         
-    [InlineData("   ")]      
-    [InlineData(null)]       
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData(null)]
     public void GetGame_ShouldReturnNull_WhenGameDoesNotExist(string? code)
     {
         var result = _manager.GetGame(code!);
-        
+
         result.Should().BeNull();
     }
 }
