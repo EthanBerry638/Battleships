136d9c131b98c18493e2e8fdca5209e4c86cf75d
EthanBerry638 <ethanberry670@gmail.com>
2026-07-14 20:39:52 +0100

    Feat: Add player tests and first test to verify behaviour. Note: Other tests still not passing but this is.

---
+++ Battleship.Tests/Unit Tests/Player Tests/PlayerTests.cs
@@ -0,0 +1,18 @@
+﻿using Battleship.Api.GamePieces.Entities;
+using FluentAssertions;
+
+namespace Battleship.Tests.Unit_Tests.Player_Tests;
+
+public class PlayerTests
+{
+    [Fact]
+    public void PlayerConstructor_ShouldThrowArgumentException_WhenIdIsEmpty()
+    {
+        var act = () => new Player(Guid.Empty, "Test Player");
+        
+        act.Should()
+            .Throw<ArgumentException>()
+            .WithParameterName("id")
+            .WithMessage("Id cannot be empty*");
+    }
+}
