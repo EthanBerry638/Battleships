50c756cc90632bb8f86431b1dc9d2555eeeca83b
EthanBerry638 <ethanberry670@gmail.com>
2026-07-19 13:33:15 +0100

    Update: Change contract of IBattleshipManager to include DisconnectResponse return type

--- Battleship.Api/Services/IBattleshipManager.cs
+++ Battleship.Api/Services/IBattleshipManager.cs
@@ -9,5 +9,5 @@ public interface IBattleshipManager
     string CreateLobby(Player player);
     BattleshipEngine? JoinLobby(string gameCode, Player player2);
     bool AddConnection(AddConnectionRequest request);
-    Task HandleDisconnectAsync(string connectionId, TimeSpan delay = default);
+    Task<DisconnectResponse> HandleDisconnectAsync(string connectionId, TimeSpan delay = default);
 }
