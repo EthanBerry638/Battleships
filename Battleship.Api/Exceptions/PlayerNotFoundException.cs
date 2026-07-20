33cf383405d8dd4f7ef65e706876cc3cb08ccf37
EthanBerry638 <ethanberry670@gmail.com>
2026-07-20 21:39:53 +0100

    Feat: Add new exception type

---
+++ Battleship.Api/Exceptions/PlayerNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Battleship.Api.Exceptions;
+
+public class PlayerNotFoundException : Exception
+{
+    public PlayerNotFoundException(){}
+
+    public PlayerNotFoundException(string message) : base(message){}
+}
