82452e5d28c66943b60218a53f6e926525a9f7c6
EthanBerry638 <ethanberry670@gmail.com>
2026-07-21 15:37:30 +0100

    Feat: Add new custom exception

---
+++ Battleship.Api/Exceptions/InvalidGameStateException.cs
@@ -0,0 +1,8 @@
+﻿namespace Battleship.Api.Exceptions;
+
+public class InvalidGameStateException : Exception
+{
+    public InvalidGameStateException(){}
+    
+    public InvalidGameStateException(string message) : base(message){}
+}
