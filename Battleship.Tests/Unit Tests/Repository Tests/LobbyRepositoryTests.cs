1c570be659c49550af8e2c251212cb9de8ea7a1a
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 14:52:33 +0100

    Feat: Add new failing tests for TryRemoveLobby

--- Battleship.Tests/Unit Tests/Repository Tests/LobbyRepositoryTests.cs
+++ Battleship.Tests/Unit Tests/Repository Tests/LobbyRepositoryTests.cs
@@ -28,4 +28,35 @@ public class LobbyRepositoryTests
 
         result.Should().BeFalse();
     }
+    
+    [Fact]
+    public void TryRemoveLobby_ShouldReturnTrueAndOutputPlayer_WhenLobbyExists()
+    {
+        _lobbyRepository.TryAddLobby("ABC123", _player1);
+
+        bool result = _lobbyRepository.TryRemoveLobby("ABC123", out Player removedPlayer);
+
+        result.Should().BeTrue();
+        removedPlayer.Should().Be(_player1);
+    }
+
+    [Fact]
+    public void TryRemoveLobby_ShouldReturnFalse_WhenLobbyDoesNotExist()
+    {
+        bool result = _lobbyRepository.TryRemoveLobby("ABC123", out Player removedPlayer);
+
+        result.Should().BeFalse();
+        removedPlayer.Should().BeNull();
+    }
+
+    [Fact]
+    public void TryRemoveLobby_ShouldAllowCodeReuse_WhenLobbyHasBeenRemoved()
+    {
+        _lobbyRepository.TryAddLobby("ABC123", _player1);
+        _lobbyRepository.TryRemoveLobby("ABC123", out _);
+
+        bool result = _lobbyRepository.TryAddLobby("ABC123", _player2);
+
+        result.Should().BeTrue();
+    }
 }
