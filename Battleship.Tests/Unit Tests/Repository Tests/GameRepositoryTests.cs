dcc31fde253f996dd11036435b3f6cb6ca466ccc
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 21:26:35 +0100

    Feat: Add private helper to create game sessions

--- Battleship.Tests/Unit Tests/Repository Tests/GameRepositoryTests.cs
+++ Battleship.Tests/Unit Tests/Repository Tests/GameRepositoryTests.cs
@@ -1,5 +1,8 @@
-﻿using Battleship.Api.GamePieces.Entities;
+﻿using Battleship.Api.Engine;
+using Battleship.Api.GamePieces.Entities;
+using Battleship.Api.GamePieces.Board;  
 using Battleship.Api.Repositories;
+using Battleship.Api.Services;
 using FluentAssertions;
 
 namespace Battleship.Tests.Unit_Tests.Repository_Tests;
@@ -10,10 +13,16 @@ public class GameRepositoryTests
     private readonly Player _player1 = new(Guid.NewGuid(), "Player 1");
     private readonly Player _player2 = new(Guid.NewGuid(), "Player 2");
     
+    private static GameSession CreateSession(Player playerOne, Player playerTwo)
+    {
+        BattleshipEngine engine = new(new GameBoard(), new GameBoard(), playerOne, playerTwo);
+        return new GameSession(engine);
+    }
+    
     [Fact]
     public void TryAddGame_ShouldReturnTrue_WhenGameDoesNotExist()
     {
-        bool result = _gameRepository.TryAddGame("ABC123", _player1);
+        bool result = _gameRepository.TryAddGame("ABC123", CreateSession(_player1, _player2));
 
         result.Should().BeTrue();
     }
@@ -21,9 +30,9 @@ public class GameRepositoryTests
     [Fact]
     public void TryAddGame_ShouldReturnFalse_WhenCodeAlreadyExists()
     {
-        _gameRepository.TryAddGame("ABC123", _player1);
+        _gameRepository.TryAddGame("ABC123", CreateSession(_player1, _player2));
 
-        bool result = _gameRepository.TryAddGame("ABC123", _player2);
+        bool result = _gameRepository.TryAddGame("ABC123", CreateSession(_player2, _player1));
 
         result.Should().BeFalse();
     }
