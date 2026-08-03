63217d9326efc6c7c2512bc5f0c3d7bfe0e8a73d
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 14:25:28 +0100

    Feat: Add new failing tests for try add connection

--- Battleship.Tests/Unit Tests/Repository Tests/ConnectionRepositoryTests.cs
+++ Battleship.Tests/Unit Tests/Repository Tests/ConnectionRepositoryTests.cs
@@ -1,8 +1,31 @@
 ﻿using Battleship.Api.Repositories;
+using Battleship.Api.DTOs;
+using FluentAssertions;
 
 namespace Battleship.Tests.Unit_Tests.Repository_Tests;
 
 public class ConnectionRepositoryTests
 {
     private readonly ConnectionRepository _connectionRepository = new();
+    
+    [Fact]
+    public void TryAddConnection_ShouldReturnTrue_WhenConnectionDoesNotExist()
+    {
+        var request = new AddConnectionRequest("connection-1", Guid.NewGuid());
+
+        bool result = _connectionRepository.TryAddConnection(request);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void TryAddConnection_ShouldReturnFalse_WhenConnectionAlreadyExists()
+    {
+        var request = new AddConnectionRequest("connection-1", Guid.NewGuid());
+        _connectionRepository.TryAddConnection(request);
+
+        bool result = _connectionRepository.TryAddConnection(request);
+
+        result.Should().BeFalse();
+    }
 }
