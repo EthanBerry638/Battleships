7241ca425287349c3efe3686daa31aa55458aa1c
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 21:13:22 +0100

    Feat: Add method stubs for testing for game repository

---
+++ Battleship.Api/Repositories/GameRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+using Battleship.Api.GamePieces.Entities;
+using Battleship.Api.Services;
+
+namespace Battleship.Api.Repositories;
+
+public class GameRepository : IGameRepository
+{
+    private readonly ConcurrentDictionary<string, GameSession> _games = new();
+
+    public bool TryAddGame(string gameCode, Player player)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public bool TryRemoveGame(string gameCode, out Player? player)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public bool TryFindKeyByPlayer(Guid playerId, out string? gameCode)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public bool GetGameByCode(string gameCode, out GameSession? game)
+    {
+        throw new NotImplementedException();
+    }   
+    
+    public bool IsPlayerInGame(Guid playerId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Battleship.Api/Repositories/IGameRepository.cs
@@ -0,0 +1,13 @@
+﻿using Battleship.Api.GamePieces.Entities;
+using Battleship.Api.Services;
+
+namespace Battleship.Api.Repositories;
+
+public interface IGameRepository
+{
+    bool TryAddGame(string gameCode, Player player);
+    bool TryRemoveGame(string gameCode, out Player? player);
+    bool TryFindKeyByPlayer(Guid playerId, out string? gameCode);
+    bool GetGameByCode(string gameCode, out GameSession? game);
+    bool IsPlayerInGame(Guid playerId);
+}
