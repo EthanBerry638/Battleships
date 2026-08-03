43389d4e2de83a87035994e5f010bfb02afcd1ac
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 14:44:32 +0100

    Feat: Add basic setup for lobby repository testing

---
+++ Battleship.Tests/Unit Tests/Repository Tests/LobbyRepository.cs
@@ -0,0 +1,10 @@
+﻿using Battleship.Api.GamePieces.Entities;
+
+namespace Battleship.Tests.Unit_Tests.Repository_Tests;
+
+public class LobbyRepository
+{
+    private readonly LobbyRepository _lobbyRepository = new();
+    private readonly Player _player1 = new(Guid.NewGuid(), "Player 1");
+    private readonly Player _player2 = new(Guid.NewGuid(), "Player 2");
+}
