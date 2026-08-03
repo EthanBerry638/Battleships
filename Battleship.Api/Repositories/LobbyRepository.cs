0b539c12ae0112c6743a9cf7fd606fe99ef7a117
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 14:42:50 +0100

    Feat: Add basic outline of Lobby Repository for testing

---
+++ Battleship.Api/Repositories/LobbyRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+using Battleship.Api.GamePieces.Entities;
+
+namespace Battleship.Api.Repositories;
+
+public class LobbyRepository : ILobbyRepository
+{
+    private readonly ConcurrentDictionary<string, Player> _lobbies = new ();
+
+    public bool TryAddLobby(string gameCode, Player player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryRemoveLobby(string gameCod, out Player player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryFindCodeByPlayer(Guid playerId, out string gameCode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsPlayerInLobby(Guid playerId)
+    {
+        throw new NotImplementedException();
+    }
+}
