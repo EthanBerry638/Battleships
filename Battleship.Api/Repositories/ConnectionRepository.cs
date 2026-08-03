9d4276e4e1a4d87a8979bdbf4ab0cd4eb01f17ac
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 14:22:41 +0100

    Feat: Add placeholder for ConnectionRepository and ConnectionRepositoryTests

---
+++ Battleship.Api/Repositories/ConnectionRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Concurrent;
+
+namespace Battleship.Api.Repositories;
+
+public class ConnectionRepository
+{
+    private readonly ConcurrentDictionary<string, Guid> _connections = new();
+}
---
+++ Battleship.Tests/Unit Tests/Repository Tests/ConnectionRepositoryTests.cs
@@ -0,0 +1,8 @@
+﻿using Battleship.Api.Repositories;
+
+namespace Battleship.Tests.Unit_Tests.Repository_Tests;
+
+public class ConnectionRepositoryTests
+{
+    private readonly ConnectionRepository _connectionRepository = new();
+}
