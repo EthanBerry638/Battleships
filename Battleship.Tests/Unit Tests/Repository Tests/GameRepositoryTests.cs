6276560448032d677cb9a0c845a150cb00977ce2
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 21:15:50 +0100

    Feat: Add basic setup for game repository testing

---
+++ Battleship.Tests/Unit Tests/Repository Tests/GameRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using Battleship.Api.GamePieces.Entities;
+using Battleship.Api.Repositories;
+using FluentAssertions;
+
+namespace Battleship.Tests.Unit_Tests.Repository_Tests;
+
+public class GameRepositoryTests
+{
+    private readonly GameRepository _gameRepository = new();
+    private readonly Player _player1 = new(Guid.NewGuid(), "Player 1");
+    private readonly Player _player2 = new(Guid.NewGuid(), "Player 2");
+}
