f8f67f80b392b5b13551b158dbe9f420de1f023a
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 14:49:53 +0100

    Feat: Add new failing tests for try add lobby

--- Battleship.Tests/Unit Tests/Repository Tests/LobbyRepository.cs
+++ Battleship.Tests/Unit Tests/Repository Tests/LobbyRepository.cs
@@ -1,10 +1,31 @@
 ﻿using Battleship.Api.GamePieces.Entities;
+using Battleship.Api.Repositories;
+using FluentAssertions;
 
 namespace Battleship.Tests.Unit_Tests.Repository_Tests;
 
-public class LobbyRepository
+public class LobbyRepositoryTests
 {
     private readonly LobbyRepository _lobbyRepository = new();
     private readonly Player _player1 = new(Guid.NewGuid(), "Player 1");
     private readonly Player _player2 = new(Guid.NewGuid(), "Player 2");
+    
+    
+    [Fact]
+    public void TryAddLobby_ShouldReturnTrue_WhenLobbyDoesNotExist()
+    {
+        bool result = _lobbyRepository.TryAddLobby("ABC123", _player1);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void TryAddLobby_ShouldReturnFalse_WhenCodeAlreadyExists()
+    {
+        _lobbyRepository.TryAddLobby("ABC123", _player1);
+
+        bool result = _lobbyRepository.TryAddLobby("ABC123", _player2);
+
+        result.Should().BeFalse();
+    }
 }
