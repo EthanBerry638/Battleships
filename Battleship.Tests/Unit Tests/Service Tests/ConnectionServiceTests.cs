6b0b81a857046f25167f9378ec1fe464a0911c30
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 21:58:18 +0100

    Feat: Add basic setup for connection service tests

---
+++ Battleship.Tests/Unit Tests/Service Tests/ConnectionServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Battleship.Api.Services;
+using Battleship.Api.Repositories;
+using Moq;
+using FluentAssertions;
+
+namespace Battleship.Tests.Unit_Tests.Service_Tests;
+
+public class ConnectionServiceTests
+{
+    private readonly ConnectionService _connectionService;
+
+    public ConnectionServiceTests()
+    {
+        var connectionRepositoryMock = new Mock<IConnectionRepository>();
+        _connectionService = new ConnectionService(connectionRepositoryMock.Object);
+    }
+}
