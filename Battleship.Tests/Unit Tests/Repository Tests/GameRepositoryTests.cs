45298a278652bf37078d435cd9bfdfd7cec98463
Ethan Berry <ethanberry670@gmail.com>
2026-08-03 21:16:34 +0100

    Feat: Add new failing tests for TryAddGame

--- Battleship.Tests/Unit Tests/Repository Tests/GameRepositoryTests.cs
+++ Battleship.Tests/Unit Tests/Repository Tests/GameRepositoryTests.cs
@@ -9,4 +9,22 @@ public class GameRepositoryTests
     private readonly GameRepository _gameRepository = new();
     private readonly Player _player1 = new(Guid.NewGuid(), "Player 1");
     private readonly Player _player2 = new(Guid.NewGuid(), "Player 2");
+    
+    [Fact]
+    public void TryAddGame_ShouldReturnTrue_WhenGameDoesNotExist()
+    {
+        bool result = _gameRepository.TryAddGame("ABC123", _player1);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void TryAddGame_ShouldReturnFalse_WhenCodeAlreadyExists()
+    {
+        _gameRepository.TryAddGame("ABC123", _player1);
+
+        bool result = _gameRepository.TryAddGame("ABC123", _player2);
+
+        result.Should().BeFalse();
+    }
 }
