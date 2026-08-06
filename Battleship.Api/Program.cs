2b4d4c93a5822cb10ab7657ea7765720c49d1129
Ethan Berry <ethanberry670@gmail.com>
2026-08-06 22:22:57 +0100

    Feat: Register repositories and services in DI container

--- Battleship.Api/Program.cs
+++ Battleship.Api/Program.cs
@@ -1,6 +1,15 @@
 using Battleship.Api.Hubs;
+using Battleship.Api.Repositories;
+using Battleship.Api.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ILobbyRepository, LobbyRepository>();
+builder.Services.AddSingleton<IGameRepository, GameRepository>();
+builder.Services.AddSingleton<IConnectionRepository, ConnectionRepository>();
+
+builder.Services.AddTransient<SessionService>();
+builder.Services.AddTransient<ConnectionService>();
 
 builder.Services.AddSignalR();
 
