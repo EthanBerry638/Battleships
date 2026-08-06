a0e167d5a4e4e4437d68f49384d8e226a1b88dae
Ethan Berry <ethanberry670@gmail.com>
2026-08-06 18:33:17 +0100

    Feat: Add generate code method copied from BattleshipManager

--- Battleship.Api/Services/SessionService.cs
+++ Battleship.Api/Services/SessionService.cs
@@ -6,4 +6,9 @@ public class SessionService (ILobbyRepository lobbyRepository, IGameRepository g
 {
     private readonly ILobbyRepository _lobbyRepository = lobbyRepository;
     private readonly IGameRepository _gameRepository = gameRepository;
+    
+    protected virtual string GenerateCode()
+    {
+        return Guid.NewGuid().ToString("N")[..6].ToUpper();
+    }
 }
