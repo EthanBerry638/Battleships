5e3bbf56e396f871ab1adeac312cde6938678085
Ethan Berry <ethanberry670@gmail.com>
2026-08-06 17:42:58 +0100

    Feat: Add basic setup for lobby service

---
+++ Battleship.Api/Services/LobbyService.cs
@@ -0,0 +1,8 @@
+﻿using Battleship.Api.Repositories;
+
+namespace Battleship.Api.Services;
+
+public class LobbyService (ILobbyRepository lobbyRepository)
+{
+    private readonly ILobbyRepository _lobbyRepository = lobbyRepository;
+}
