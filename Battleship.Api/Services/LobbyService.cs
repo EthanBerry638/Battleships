6b8d2aa023d477155f70767823bc8395345851cb
Ethan Berry <ethanberry670@gmail.com>
2026-08-06 18:29:17 +0100

    Update: Rename LobbyService to SessionService and inject Game repository

--- Battleship.Api/Services/LobbyService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Battleship.Api.Repositories;
-
-namespace Battleship.Api.Services;
-
-public class LobbyService (ILobbyRepository lobbyRepository)
-{
-    private readonly ILobbyRepository _lobbyRepository = lobbyRepository;
-}
---
+++ Battleship.Api/Services/SessionService.cs
@@ -0,0 +1,9 @@
+﻿using Battleship.Api.Repositories;
+
+namespace Battleship.Api.Services;
+
+public class SessionService (ILobbyRepository lobbyRepository, IGameRepository gameRepository)
+{
+    private readonly ILobbyRepository _lobbyRepository = lobbyRepository;
+    private readonly IGameRepository _gameRepository = gameRepository;
+}
