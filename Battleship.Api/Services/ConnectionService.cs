c61a46a982e86c9b6b518de425b71b712462d2b1
Ethan Berry <ethanberry670@gmail.com>
2026-08-06 11:15:32 +0100

    Fix: Add ConnectionService implementation for AddConnection

---
+++ Battleship.Api/Services/ConnectionService.cs
@@ -0,0 +1,18 @@
+﻿using Battleship.Api.Repositories;
+using Battleship.Api.DTOs;
+
+namespace Battleship.Api.Services;
+
+public class ConnectionService (IConnectionRepository connectionRepository)
+{
+    private readonly IConnectionRepository _connectionRepository = connectionRepository;
+    
+    public bool AddConnection(AddConnectionRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        if (string.IsNullOrWhiteSpace(request.ConnectionId) || request.PlayerId == Guid.Empty)
+            throw new ArgumentException("ConnectionId and/or Guid cannot be null or empty.");
+
+        return _connectionRepository.TryAddConnection(request);
+    }
+}
