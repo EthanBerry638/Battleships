7b72d37fe2158dd851f64dfcdceba731c4737a57
Ethan Berry <ethanberry670@gmail.com>
2026-08-06 22:24:43 +0100

    Feat: Add basic setup for game service and register in Program.cs

---
+++ Battleship.Api/Services/GameService.cs
@@ -0,0 +1,8 @@
+﻿using Battleship.Api.Repositories;
+
+namespace Battleship.Api.Services;
+
+public class GameService (IGameRepository gameRepository)
+{
+    private readonly IGameRepository _gameRepository = gameRepository;
+}
