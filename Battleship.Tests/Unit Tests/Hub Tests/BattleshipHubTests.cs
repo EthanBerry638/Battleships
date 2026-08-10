03a793ef5cb79c63941473711829c11de2d0f781
Ethan Berry <ethanberry670@gmail.com>
2026-08-10 15:54:49 +0100

    Update: Apply rename GameStartedMessage to GameCreatedMessage

--- Battleship.Tests/Unit Tests/Hub Tests/BattleshipHubTests.cs
+++ Battleship.Tests/Unit Tests/Hub Tests/BattleshipHubTests.cs
@@ -100,10 +100,10 @@ public class BattleshipHubTests
                 "GameStarted",
                 It.Is<object[]>(args =>
                     args.Length == 1 &&
-                    args[0] is GameStartedMessage &&
-                    ((GameStartedMessage)args[0]).StartingPlayer == expectedEngine.CurrentPlayer &&
-                    ((GameStartedMessage)args[0]).Player1Id == expectedEngine.Players[0].Id &&
-                    ((GameStartedMessage)args[0]).Player2Id == expectedEngine.Players[1].Id),
+                    args[0] is GameCreatedMessage &&
+                    ((GameCreatedMessage)args[0]).StartingPlayer == expectedEngine.CurrentPlayer &&
+                    ((GameCreatedMessage)args[0]).Player1Id == expectedEngine.Players[0].Id &&
+                    ((GameCreatedMessage)args[0]).Player2Id == expectedEngine.Players[1].Id),
                 CancellationToken.None),
             Times.Once);
     }
