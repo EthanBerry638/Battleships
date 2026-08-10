30d2eaf493026ed8f11154943b444390e1225abb
Ethan Berry <ethanberry670@gmail.com>
2026-08-10 13:15:59 +0100

    Feat: Add GameStartedMessage DTO

---
+++ Battleship.Api/DTOs/GameStartedMessage.cs
@@ -0,0 +1,8 @@
+﻿using Battleship.Api.GamePieces.Entities;
+
+namespace Battleship.Api.DTOs;
+
+public record GameStartedMessage(
+    Player StartingPlayer,
+    Guid Player1Id,
+    Guid Player2Id);
