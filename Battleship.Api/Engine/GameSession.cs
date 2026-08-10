e3eb1146705313e2ecf3d69c9b5e9b5f9695a32d
Ethan Berry <ethanberry670@gmail.com>
2026-08-10 15:56:22 +0100

    Update: Add property BothPlayersReady to track player readiness status

--- Battleship.Api/Engine/GameSession.cs
+++ Battleship.Api/Engine/GameSession.cs
@@ -2,13 +2,20 @@
 
 public class GameSession
 {
+    private readonly HashSet<Guid> _readyPlayers = [];
     public object Lock { get; } = new();
     public BattleshipEngine Engine { get; }
+    public bool BothPlayersReady => _readyPlayers.Count == 2;
 
     public GameSession(BattleshipEngine engine)
     {
         ArgumentNullException.ThrowIfNull(engine);
         
         Engine = engine;
+    }
+
+    public void SetPlayerReady(Guid playerId)
+    { 
+        _readyPlayers.Add(playerId);
     }
 }
