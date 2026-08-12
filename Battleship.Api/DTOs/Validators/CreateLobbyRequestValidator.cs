fe225bcdd124d729f6f0ddebc8c9298ab2f9d0cf
Ethan Berry <ethanberry670@gmail.com>
2026-08-12 10:54:02 +0100

    Feat: Add CreateLobbyRequestValidator tests and implementation

---
+++ Battleship.Api/DTOs/Validators/CreateLobbyRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Battleship.Api.DTOs.Validators;
+
+public class CreateLobbyRequestValidator : AbstractValidator<CreateLobbyRequest>
+{
+    public CreateLobbyRequestValidator()
+    {
+        RuleFor(x => x.PlayerId)
+            .NotEmpty()
+            .WithMessage("PlayerId is required.");
+
+        RuleFor(x => x.PlayerName)
+            .NotEmpty()
+            .WithMessage("Player name is required.");
+    }
+}
