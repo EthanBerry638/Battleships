cf503faeed30acb87876698a87d45c7933f4dcf5
Ethan Berry <ethanberry670@gmail.com>
2026-08-12 11:47:45 +0100

    Feat: Add TryStartGameRequestValidator and corresponding tests

---
+++ Battleship.Api/DTOs/Validators/TryStartGameRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Battleship.Api.DTOs.Requests;
+
+namespace Battleship.Api.DTOs.Validators;
+
+public class TryStartGameRequestValidator : AbstractValidator<TryStartGameRequest>
+{
+    public TryStartGameRequestValidator()
+    {
+        RuleFor(x => x.PlayerId)
+            .NotEmpty()
+            .WithMessage("PlayerId is required");
+    }
+}
