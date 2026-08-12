86c96cc878ac04b3c52a420631d1582eef51bbc9
Ethan Berry <ethanberry670@gmail.com>
2026-08-12 14:28:37 +0100

    Feat: Register ValidationHubFilter for signalR

--- Battleship.Api/Program.cs
+++ Battleship.Api/Program.cs
@@ -2,7 +2,9 @@ using Battleship.Api.Hubs;
 using Battleship.Api.Repositories;
 using Battleship.Api.Services;
 using Battleship.Api.DTOs.Validators;
+using Battleship.Api.Hubs.Filters;
 using FluentValidation;
+using Microsoft.AspNetCore.SignalR;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +18,10 @@ builder.Services.AddTransient<IConnectionService, ConnectionService>();
 
 builder.Services.AddValidatorsFromAssemblyContaining<CreateLobbyRequestValidator>();
 
-builder.Services.AddSignalR();
+builder.Services.AddSignalR(options =>
+{
+    options.AddFilter<ValidationHubFilter>();         
+});
 
 WebApplication app = builder.Build();
 
