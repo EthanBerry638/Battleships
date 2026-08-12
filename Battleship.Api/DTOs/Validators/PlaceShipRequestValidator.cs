9eb8e675ef1c6b732a6c19559e7a6749613ae007
Ethan Berry <ethanberry670@gmail.com>
2026-08-12 11:40:42 +0100

    Feat: Add tests and implementation for PlaceShipRequestValidator

---
+++ Battleship.Api/DTOs/Validators/PlaceShipRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using Battleship.Api.DTOs.Requests;
+using FluentValidation;
+
+namespace Battleship.Api.DTOs.Validators;
+
+public class PlaceShipRequestValidator : AbstractValidator<PlaceShipRequest>
+{
+    public PlaceShipRequestValidator()
+    {
+        RuleFor(x => x.PlayerId)
+            .NotEmpty()
+            .WithMessage("PlayerId is required.");
+
+        RuleFor(x => x.Type)
+            .IsInEnum()
+            .WithMessage("Ship type must be a valid ship type.");
+
+        RuleFor(x => x.Coordinates)
+            .NotEmpty()
+            .WithMessage("Coordinates are required.");
+
+        RuleForEach(x => x.Coordinates)
+            .NotNull()
+            .WithMessage("Coordinates cannot contain null entries.");
+    }
+}
