b34d2ccb0540ce07c57b6601a40bfa5045c2b6e1
Ethan Berry <ethanberry670@gmail.com>
2026-08-12 11:59:36 +0100

    Feat: Add GetWinnerRequestValidator and corresponding tests

---
+++ Battleship.Api/DTOs/Validators/GetWinnerRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Battleship.Api.DTOs.Requests;
+
+namespace Battleship.Api.DTOs.Validators;
+
+public class GetWinnerRequestValidator : AbstractValidator<GetWinnerRequest>
+{
+    public GetWinnerRequestValidator()
+    {
+        RuleFor(x => x.GameCode)
+            .NotEmpty()
+            .WithMessage("GameCode is required");
+    }
+}
---
+++ Battleship.Tests/Unit Tests/Validator Tests/GetWinnerRequestValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using Battleship.Api.DTOs.Requests;
+using Battleship.Api.DTOs.Validators;
+using FluentValidation.TestHelper;
+
+namespace Battleship.Tests.Unit_Tests.Validator_Tests;
+
+public class GetWinnerRequestValidatorTests
+{
+    private readonly GetWinnerRequestValidator _validator = new();
+
+    [Fact]
+    public void ShouldNotHaveError_WhenGameCodeIsValid()
+    {
+        var request = new GetWinnerRequest("GAME1");
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void ShouldHaveError_WhenGameCodeIsNullOrWhitespace(string? gameCode)
+    {
+        var request = new GetWinnerRequest(gameCode!);
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldHaveValidationErrorFor(x => x.GameCode)
+            .WithErrorMessage("GameCode is required");
+    }
+}
