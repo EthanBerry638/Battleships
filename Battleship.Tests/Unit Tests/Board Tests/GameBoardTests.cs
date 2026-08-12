1627f95664ca35f1986485cc0f5e7c24b330a83c
Ethan Berry <ethanberry670@gmail.com>
2026-08-12 10:22:57 +0100

    Update: Delete uneeded test

--- Battleship.Tests/Unit Tests/Board Tests/GameBoardTests.cs
+++ Battleship.Tests/Unit Tests/Board Tests/GameBoardTests.cs
@@ -42,24 +42,6 @@ public class GameBoardTests
         secondResult.Should().BeSameAs(firstResult);
     }
 
-    [Theory]
-    [InlineData(-1, 0)]
-    [InlineData(10, 0)]
-    [InlineData(0, -1)]
-    [InlineData(0, 10)]
-    [InlineData(-1, -1)]
-    [InlineData(10, 10)]
-    public void GetTile_ThrowsException_WhenCoordinateIsOutsideBoard(int x, int y)
-    {
-        var gameBoard = new GameBoard();
-        var coordinate = new Coordinate(x, y);
-
-        var action = () => gameBoard.GetTile(coordinate);
-
-        action.Should().Throw<InvalidCoordinateException>()
-            .WithMessage($"Invalid coordinate: {coordinate}");
-    }
-
     [Fact]
     public void PlaceShip_ReturnsSuccess_WhenTilesAreEmpty()
     {
