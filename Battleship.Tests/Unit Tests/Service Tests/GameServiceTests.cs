89f7e35bbf18da0b5b6fffec7171535335f2fc64
Ethan Berry <ethanberry670@gmail.com>
2026-08-16 22:42:46 +0100

    Update: Simplified test and added legal coordinates for ship

--- Battleship.Tests/Unit Tests/Service Tests/GameServiceTests.cs
+++ Battleship.Tests/Unit Tests/Service Tests/GameServiceTests.cs
@@ -189,11 +189,10 @@ public class GameServiceTests
         var (session, player1, _, board1Mock, _) = CreateSession();
         string gameCode = "GAME1";
         SetupPlayerFoundInGame(player1.Id, gameCode, session);
-        board1Mock.Setup(b => b.ValidateFleet()).Returns(new FleetValidationResult(true, [], []));
-        _gameService.ValidateFleet(player1.Id);
+        session.SetPlayerReady(player1.Id);
         
         var act = () => _gameService.PlaceShip(new PlaceShipRequest(player1.Id, ShipType.PatrolBoat,
-            [new Coordinate(0, 0)]));
+            [new Coordinate(0, 0), new Coordinate(0, 1)]));
         
         act.Should().Throw<FleetLockedException>()
             .WithMessage("You can't place a ship after readying.");
