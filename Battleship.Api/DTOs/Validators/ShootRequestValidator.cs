e35c51b259e75bc1bb0cd4f8f639d60d253483e3
Ethan Berry <ethanberry670@gmail.com>
2026-08-16 23:37:06 +0100

    Feat: Add shoot request validtor and tests

---
+++ Battleship.Api/DTOs/Validators/ShootRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using Battleship.Api.DTOs.Requests;
+using FluentValidation;
+
+namespace Battleship.Api.DTOs.Validators;
+
+public class ShootRequestValidator : AbstractValidator<ShootRequest>
+{
+    public ShootRequestValidator()
+    {
+        RuleFor(x => x.PlayerId)
+            .NotEmpty()
+            .WithMessage("PlayerId is required.");
+
+        RuleFor(x => x.Coordinate)
+            .NotNull()
+            .WithMessage("Coordinate is required.");
+    }
+}
