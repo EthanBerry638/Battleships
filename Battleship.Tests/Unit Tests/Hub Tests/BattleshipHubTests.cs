96c252111129160afaff30946b7102834187be69
Ethan Berry <ethanberry670@gmail.com>
2026-08-17 00:19:08 +0100

    Update: Collapsed Facts into one theory

--- Battleship.Tests/Unit Tests/Hub Tests/BattleshipHubTests.cs
+++ Battleship.Tests/Unit Tests/Hub Tests/BattleshipHubTests.cs
@@ -292,14 +292,22 @@ public class BattleshipHubTests
         _mockGameService.Verify(g => g.ValidateFleet(playerId), Times.Once);
     }
     
-    [Fact]
-    public async Task Shoot_ShouldSendHitMessageToGroup_WhenCalled()
+    [Theory]
+    [InlineData(ShotResult.Hit, 3, 5)]
+    [InlineData(ShotResult.Miss, 0, 0)]
+    [InlineData(ShotResult.Duplicate,0, 0)]
+    [InlineData(ShotResult.Sunk, 0, 0)]
+    public async Task Shoot_ShouldSendMessageToGroup_WhenShotIsProcessed(
+        ShotResult expectedResult, 
+        int x, 
+        int y)
     {
         var playerId = Guid.NewGuid();
         string gameCode = "ABC123";
-        var coordinate = new Coordinate(3, 5);
+        var coordinate = new Coordinate(x, y);
         var request = new ShootRequest(playerId, coordinate);
-        var response = new ShotResponse(ShotResult.Hit, gameCode, playerId, coordinate);
+        var response = new ShotResponse(expectedResult, gameCode, playerId, coordinate);
+
         _mockGameService.Setup(g => g.Shoot(playerId, coordinate)).Returns(response);
         _mockClients.Setup(c => c.Group(gameCode)).Returns(_mockClientProxy.Object);
 
@@ -313,91 +321,7 @@ public class BattleshipHubTests
                 It.Is<object[]>(args =>
                     args.Length == 1 &&
                     args[0] is ShotMessage &&
-                    ((ShotMessage)args[0]).Result == ShotResult.Hit &&
-                    ((ShotMessage)args[0]).ShooterId == playerId &&
-                    ((ShotMessage)args[0]).Coordinate == coordinate),
-                CancellationToken.None),
-            Times.Once);
-    }
-
-    [Fact]
-    public async Task Shoot_ShouldSendMissMessageToGroup_WhenShotMisses()
-    {
-        var playerId = Guid.NewGuid();
-        string gameCode = "ABC123";
-        var coordinate = new Coordinate(0, 0);
-        var request = new ShootRequest(playerId, coordinate);
-        var response = new ShotResponse(ShotResult.Miss, gameCode, playerId, coordinate);
-        _mockGameService.Setup(g => g.Shoot(playerId, coordinate)).Returns(response);
-        _mockClients.Setup(c => c.Group(gameCode)).Returns(_mockClientProxy.Object);
-
-        await CreateHub().Shoot(request);
-
-        _mockGameService.Verify(g => g.Shoot(playerId, coordinate), Times.Once);
-        _mockClients.Verify(c => c.Group(gameCode), Times.Once);
-        _mockClientProxy.Verify(
-            p => p.SendCoreAsync(
-                "Shot",
-                It.Is<object[]>(args =>
-                    args.Length == 1 &&
-                    args[0] is ShotMessage &&
-                    ((ShotMessage)args[0]).Result == ShotResult.Miss &&
-                    ((ShotMessage)args[0]).ShooterId == playerId &&
-                    ((ShotMessage)args[0]).Coordinate == coordinate),
-                CancellationToken.None),
-            Times.Once);
-    }
-    
-    [Fact]
-    public async Task Shoot_ShouldSendDuplicateMessageToGroup_WhenShotIsADuplicate()
-    {
-        var playerId = Guid.NewGuid();
-        var coordinate = new Coordinate(0, 0);
-        string gameCode = "XYZ789";
-        var request = new ShootRequest(playerId, coordinate);
-        var response = new ShotResponse(ShotResult.Duplicate, gameCode, playerId, coordinate);
-        _mockGameService.Setup(g => g.Shoot(playerId, coordinate)).Returns(response);
-        _mockClients.Setup(c => c.Group(gameCode)).Returns(_mockClientProxy.Object);
-
-        await CreateHub().Shoot(request);
-
-        _mockGameService.Verify(g => g.Shoot(playerId, coordinate), Times.Once);
-        _mockClients.Verify(c => c.Group(gameCode), Times.Once);
-        _mockClientProxy.Verify(
-            p => p.SendCoreAsync(
-                "Shot",
-                It.Is<object[]>(args =>
-                    args.Length == 1 &&
-                    args[0] is ShotMessage &&
-                    ((ShotMessage)args[0]).Result == ShotResult.Duplicate &&
-                    ((ShotMessage)args[0]).ShooterId == playerId &&
-                    ((ShotMessage)args[0]).Coordinate == coordinate),
-                CancellationToken.None),
-            Times.Once);
-    }
-    
-    [Fact]
-    public async Task Shoot_ShouldSendSunkMessageToGroup_WhenShotIsASink()
-    {
-        var playerId = Guid.NewGuid();
-        var coordinate = new Coordinate(0, 0);
-        string gameCode = "XYZ789";
-        var request = new ShootRequest(playerId, coordinate);
-        var response = new ShotResponse(ShotResult.Sunk, gameCode, playerId, coordinate);
-        _mockGameService.Setup(g => g.Shoot(playerId, coordinate)).Returns(response);
-        _mockClients.Setup(c => c.Group(gameCode)).Returns(_mockClientProxy.Object);
-
-        await CreateHub().Shoot(request);
-
-        _mockGameService.Verify(g => g.Shoot(playerId, coordinate), Times.Once);
-        _mockClients.Verify(c => c.Group(gameCode), Times.Once);
-        _mockClientProxy.Verify(
-            p => p.SendCoreAsync(
-                "Shot",
-                It.Is<object[]>(args =>
-                    args.Length == 1 &&
-                    args[0] is ShotMessage &&
-                    ((ShotMessage)args[0]).Result == ShotResult.Sunk &&
+                    ((ShotMessage)args[0]).Result == expectedResult &&
                     ((ShotMessage)args[0]).ShooterId == playerId &&
                     ((ShotMessage)args[0]).Coordinate == coordinate),
                 CancellationToken.None),
