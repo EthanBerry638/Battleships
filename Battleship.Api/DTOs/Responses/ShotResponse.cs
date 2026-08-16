55a4389ef670f6a38673cdcf41ca287f9d6a6eac
Ethan Berry <ethanberry670@gmail.com>
2026-08-16 23:27:10 +0100

    Feat: Add response objects for Shoot

---
+++ Battleship.Api/DTOs/Responses/ShotResponse.cs
@@ -0,0 +1,9 @@
+﻿using Battleship.Api.GamePieces.Data;
+
+namespace Battleship.Api.DTOs.Responses;
+
+public record ShotResponse(
+    ShotResult Result,
+    string GameCode,
+    Guid ShooterId,
+    Coordinate Coordinate);
