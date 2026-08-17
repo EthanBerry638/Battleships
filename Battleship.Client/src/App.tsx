0888c390fc1ee87ac9093990028c76422a927641
Ethan Berry <ethanberry670@gmail.com>
2026-08-17 19:28:38 +0100

    Feat: Minimum code to allow someone to join a game and get a game code

--- Battleship.Client/src/App.tsx
+++ Battleship.Client/src/App.tsx
@@ -1,18 +1,55 @@
-import { useEffect } from 'react'
-import { startConnection } from './signalR'
+import { useEffect, useState } from 'react'
+import { connection, startConnection } from './signalR'
+
+type Screen = 'home' | 'create'
 
 function App() {
+    const [screen, setScreen] = useState<Screen>('home')
+    const [gameCode, setGameCode] = useState<string | null>(null)
+    const [playerId] = useState(() => crypto.randomUUID())
+
     useEffect(() => {
         startConnection()
             .then(() => console.log('SignalR connected'))
             .catch(console.error)
     }, [])
 
+    async function createGame() {
+        const code = await connection.invoke<string>(
+            'CreateLobby',
+            {
+                playerId,
+                playerName: 'Player 1'
+            }
+        )
+
+        setGameCode(code)
+        setScreen('create')
+    }
+
+    if (screen === 'create') {
+        return (
+            <div>
+                <h1>{gameCode}</h1>
+
+                <button onClick={() => setScreen('home')}>
+                    Back
+                </button>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h1>Battleship</h1>
-            <button>Create Game</button>
-            <button>Join Game</button>
+
+            <button onClick={createGame}>
+                Create Game
+            </button>
+
+            <button>
+                Join Game
+            </button>
         </div>
     )
 }
