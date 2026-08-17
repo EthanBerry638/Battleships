874069de277bb9b45941155e319a9c85f4b14251
Ethan Berry <ethanberry670@gmail.com>
2026-08-17 18:48:07 +0100

    Feat: Add basic setup to connect to hub

--- Battleship.Client/src/App.tsx
+++ Battleship.Client/src/App.tsx
@@ -1,13 +1,20 @@
-function App() {
-  console.log('App rendered')
+import { useEffect } from 'react'
+import { startConnection } from './signalR'
 
-  return (
-    <div>
-      <h1>Battleship</h1>
-      <button>Create Game</button>
-      <button>Join Game</button>
-    </div>
-  )
+function App() {
+    useEffect(() => {
+        startConnection()
+            .then(() => console.log('SignalR connected'))
+            .catch(console.error)
+    }, [])
+
+    return (
+        <div>
+            <h1>Battleship</h1>
+            <button>Create Game</button>
+            <button>Join Game</button>
+        </div>
+    )
 }
 
 export default App
---
+++ Battleship.Client/src/signalR.ts
@@ -0,0 +1,12 @@
+﻿import * as signalR from '@microsoft/signalr'
+
+export const connection = new signalR.HubConnectionBuilder()
+    .withUrl('https://localhost:7146/gameHub')
+    .withAutomaticReconnect()
+    .build()
+
+export async function startConnection() {
+    if (connection.state === signalR.HubConnectionState.Disconnected) {
+        await connection.start()
+    }
+}
