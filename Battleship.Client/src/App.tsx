d459fd30fda76365d7e664abec11c94507b0e49b
Ethan Berry <ethanberry670@gmail.com>
2026-08-17 18:19:35 +0100

    Feat: Add basic React setup

---
+++ Battleship.Client/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  console.log('App rendered')
+
+  return (
+    <div>
+      <h1>Battleship</h1>
+      <button>Create Game</button>
+      <button>Join Game</button>
+    </div>
+  )
+}
+
+export default App
