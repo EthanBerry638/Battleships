cd104a390d5b64d04ccccaed2cb5f1ebe3a7b021
Ethan Berry <ethanberry670@gmail.com>
2026-08-17 19:42:09 +0100

    Feat: Add basic minimal setup to test creating and joining lobbies works

--- Battleship.Client/src/App.tsx
+++ Battleship.Client/src/App.tsx
@@ -6,12 +6,23 @@ type Screen = 'home' | 'create'
 function App() {
     const [screen, setScreen] = useState<Screen>('home')
     const [gameCode, setGameCode] = useState<string | null>(null)
+    const [joinCode, setJoinCode] = useState('')
     const [playerId] = useState(() => crypto.randomUUID())
 
     useEffect(() => {
         startConnection()
             .then(() => console.log('SignalR connected'))
             .catch(console.error)
+    }, [])
+    
+    useEffect(() => {
+        connection.on('GameCreated', message => {
+            console.log('GameCreated:', message)
+        })
+
+        return () => {
+            connection.off('GameCreated')
+        }
     }, [])
 
     async function createGame() {
@@ -25,6 +36,19 @@ function App() {
 
         setGameCode(code)
         setScreen('create')
+    }
+
+    async function joinGame() {
+        const joined = await connection.invoke<boolean>(
+            'JoinLobby',
+            {
+                gameCode: joinCode,
+                playerId,
+                playerName: 'Player 2'
+            }
+        )
+
+        console.log('joined:', joined)
     }
 
     if (screen === 'create') {
@@ -47,9 +71,17 @@ function App() {
                 Create Game
             </button>
 
-            <button>
-                Join Game
-            </button>
+            <div>
+                <input
+                    value={joinCode}
+                    onChange={e => setJoinCode(e.target.value)}
+                    placeholder="Game code"
+                />
+
+                <button onClick={joinGame}>
+                    Join Game
+                </button>
+            </div>
         </div>
     )
 }
