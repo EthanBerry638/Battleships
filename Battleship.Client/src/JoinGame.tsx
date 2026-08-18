9edf982401ce1ebf42c76412f0f57a6bc76794bf
Ethan Berry <ethanberry670@gmail.com>
2026-08-18 11:15:57 +0100

    Feat: Add JoinGame component

---
+++ Battleship.Client/src/JoinGame.tsx
@@ -0,0 +1,36 @@
+﻿import { useState } from 'react';
+import { connection } from './signalR';
+
+interface JoinGameProps {
+    playerId: string;
+}
+
+function JoinGame({ playerId }: JoinGameProps) {
+    const [joinCode, setJoinCode] = useState('');
+    
+    async function joinGame() {
+        const joined = await connection.invoke<boolean>(
+            'JoinLobby',
+            {
+                gameCode: joinCode,
+                playerId,
+                playerName: 'Player 2'
+            }
+        )
+
+        console.log('joined:', joined)
+    }
+    
+    return (
+        <div>
+            <input
+                value={joinCode}
+                onChange={(e) => setJoinCode(e.target.value)}
+                placeholder="Game code"/>
+            
+            <button onClick={joinGame}>Join</button>
+        </div>
+    )
+}
+
+export default JoinGame;
