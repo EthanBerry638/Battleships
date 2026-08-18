abe8e5138af7f106f4b585538d5dbac46aa83c60
Ethan Berry <ethanberry670@gmail.com>
2026-08-18 11:41:01 +0100

    Feat: Add CreateGame component

---
+++ Battleship.Client/src/CreateGame.tsx
@@ -0,0 +1,29 @@
+﻿import { useState } from 'react';
+import { connection } from './signalR';
+
+interface CreateGameProps {
+    playerId: string;
+    onGameCreated: (gameCode: string) => void;
+}
+
+function JoinGame({ playerId, onGameCreated }: CreateGameProps) {
+    async function createGame() {
+        const code = await connection.invoke<string>(
+            'CreateLobby',
+            {
+                playerId,
+                playerName: 'Player 1'
+            }
+        )
+
+        onGameCreated(code)
+    }
+    
+    return (
+        <button onClick={createGame}>
+            Create Game
+        </button>
+    )
+}
+
+export default JoinGame;
