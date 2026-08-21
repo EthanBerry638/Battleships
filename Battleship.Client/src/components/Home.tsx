5f227ea0ab81d7b0997451c9fcc379517c21c5f8
Ethan Berry <ethanberry670@gmail.com>
2026-08-21 20:10:12 +0100

    Update: Give Home screen both onCreateGame and onJoinGame props for navigation

--- Battleship.Client/src/components/Home.tsx
+++ Battleship.Client/src/components/Home.tsx
@@ -1,7 +1,14 @@
-﻿function Home() {
+﻿interface HomeProps {
+    onCreateGame: () => void;
+    onJoinGame: () => void;
+}
+
+function Home({ onCreateGame, onJoinGame }: HomeProps) {
     return (
         <div>
             <h1>Battleship</h1>
+            <button onClick={onCreateGame}>Create Game</button>
+            <button onClick={onJoinGame}>Join Game</button>
         </div>
     );
 }
