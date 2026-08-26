994b2336931ad53ba2ed0606c3917d9b7bcf6202
Ethan Berry <ethanberry670@gmail.com>
2026-08-26 16:09:08 +0100

    Fix: Now passing all tests for connection lifecycle states

--- Battleship.Client/src/signalR.ts
+++ Battleship.Client/src/signalR.ts
@@ -1,12 +1,35 @@
 ﻿import * as signalR from '@microsoft/signalr';
 
+export type ConnectionStatus = 'connecting' | 'connected' | 'reconnecting' | 'disconnected';
+
 export const connection = new signalR.HubConnectionBuilder()
     .withUrl('https://localhost:7146/gameHub')
     .withAutomaticReconnect()
     .build();
 
-export async function startConnection() {
-  if (connection.state === signalR.HubConnectionState.Disconnected) {
-    await connection.start();
+export async function startConnection(
+    onStatusChange: (status: ConnectionStatus) => void) {
+    connection.onreconnecting(() => {
+        onStatusChange('reconnecting');
+    });
+
+    connection.onreconnected(() => {
+        onStatusChange('connected');
+    });
+
+    connection.onclose(() => {
+        onStatusChange('disconnected');
+    });
+
+    if (connection.state === signalR.HubConnectionState.Disconnected) {
+        try {
+            onStatusChange('connecting');
+
+            await connection.start();
+
+            onStatusChange('connected');
+        } catch {
+            onStatusChange('disconnected');
+        }
     }
 }
