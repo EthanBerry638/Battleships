549678433a2e2d2a93d55eba92ed965a3e7e419a
Ethan Berry <ethanberry670@gmail.com>
2026-08-27 10:57:41 +0100

    Feat: Add css and board layout for Setup screen. Will extract to it's own component

--- Battleship.Client/src/screens/Setup.tsx
+++ Battleship.Client/src/screens/Setup.tsx
@@ -1,8 +1,39 @@
-﻿function Setup () {
+﻿import "./Setup.css";
+
+const letters = "ABCDEFGHIJ".split("");
+const numbers = Array.from({ length: 10 }, (_, index) => index + 1);
+
+function Setup() {
     return (
-        <div>
-            <h1>Setup screen</h1>
-        </div>
+        <main>
+            <h1>Setup</h1>
+
+            <div className="board">
+                <div />
+
+                {numbers.map((number) => (
+                    <div className="label" key={number}>
+                        {number}
+                    </div>
+                ))}
+
+                {letters.map((letter) => (
+                    <div className="board-row" key={letter}>
+                        <div className="label">{letter}</div>
+
+                        {numbers.map((number) => (
+                            <div
+                                className="cell"
+                                key={`${letter}${number}`}
+                                title={`${letter}${number}`}
+                            >
+                                <span className="coordinate-dot" />
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </main>
     );
 }
 
