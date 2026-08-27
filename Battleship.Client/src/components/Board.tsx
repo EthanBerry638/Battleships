4ca370fb4e825abb718aa7bbc88f417ac5839195
Ethan Berry <ethanberry670@gmail.com>
2026-08-27 22:52:06 +0100

    Update: Move board css and rendering to it's own component

---
+++ Battleship.Client/src/components/Board.tsx
@@ -0,0 +1,40 @@
+﻿import "./Board.css";
+
+const letters = "ABCDEFGHIJ".split("");
+const numbers = Array.from({ length: 10 }, (_, index) => index + 1);
+
+function Board() {
+    return (
+        <main>
+            <h1>Setup</h1>
+
+            <div className="board">
+                <div />
+
+                {numbers.map((number) => (
+                    <div className="label" key={number}>
+                        {number}
+                    </div>
+                ))}
+
+                {letters.map((letter) => (
+                    <div className="board-row" key={letter}>
+                        <div className="label">{letter}</div>
+
+                        {numbers.map((number) => (
+                            <div
+                                className="cell"
+                                key={`${letter}${number}`}
+                                title={`${letter}${number}`}
+                            >
+                                <span className="coordinate-dot" />
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
+}
+
+export default Board;
--- Battleship.Client/src/screens/Setup.tsx
+++ Battleship.Client/src/screens/Setup.tsx
@@ -1,39 +1,8 @@
-﻿import "./Setup.css";
-
-const letters = "ABCDEFGHIJ".split("");
-const numbers = Array.from({ length: 10 }, (_, index) => index + 1);
+﻿import Board from "../components/Board.tsx";
 
 function Setup() {
     return (
-        <main>
-            <h1>Setup</h1>
-
-            <div className="board">
-                <div />
-
-                {numbers.map((number) => (
-                    <div className="label" key={number}>
-                        {number}
-                    </div>
-                ))}
-
-                {letters.map((letter) => (
-                    <div className="board-row" key={letter}>
-                        <div className="label">{letter}</div>
-
-                        {numbers.map((number) => (
-                            <div
-                                className="cell"
-                                key={`${letter}${number}`}
-                                title={`${letter}${number}`}
-                            >
-                                <span className="coordinate-dot" />
-                            </div>
-                        ))}
-                    </div>
-                ))}
-            </div>
-        </main>
+        <Board></Board>
     );
 }
 
