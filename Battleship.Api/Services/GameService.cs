0dfcc40afa8b641bcf67a29abfab12d573d0b26c
Ethan Berry <ethanberry670@gmail.com>
2026-09-11 19:50:54 +0100

    Feat: Add tests and implementation for ClearHub method in GameService

--- Battleship.Api/Services/GameService.cs
+++ Battleship.Api/Services/GameService.cs
@@ -102,6 +102,15 @@ public class GameService (IGameRepository gameRepository) : IGameService
 
     public bool ClearBoard(Guid playerId)
     {
-        throw new NotImplementedException();
+        if (!_gameRepository.TryFindKeyByPlayerId(playerId, out string? gameCode))
+            throw new PlayerNotFoundException($"No active game found for player with id {playerId}.");
+
+        if (!_gameRepository.TryGetGameByCode(gameCode!, out GameSession? session))
+            throw new GameNotFoundException($"Game by game code: {gameCode} not found.");
+        
+        lock (session!.Lock)
+        {
+            return session.Engine.ClearBoard(playerId);
+        }
     }
 }
